23a84bb9c030294e5358d9e262d87d3b08fa9396
Cairol <Cairol75@gmx.de>
2019-09-28 17:31:57 +0200

    trlte (eur): audio: Set sepolicy file context for mixer_paths.xml

--- releasetools/mixer_paths_eur.sh
+++ releasetools/mixer_paths_eur.sh
@@ -4,4 +4,5 @@ MIXER_PATHS=/tmp/install/bin/mixer_paths_eur.xml
 
 if [ "$VARIANT" = "xx" ] ; then
     cp -f "$MIXER_PATHS" "/system/vendor/etc/mixer_paths.xml"
+    chcon u:object_r:vendor_configs_file:s0 /system/vendor/etc/mixer_paths.xml
 fi
